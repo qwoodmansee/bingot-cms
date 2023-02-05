dcc05ae3c5737f1c418943ffabf5dadca0a01d9d
Quinton Woodmansee <qwoodmansee@capital.edu>
2023-02-04 19:30:35 -0500

    tailwind config and linting setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllPostsForHome } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPostsForHome } from '../lib/api';
+import Head from 'next/head';
 
 export default function Index({ preview, allPosts }) {
   const heroPost = allPosts[0];
