f48a73952ae34a9b27e922fe5df018abee79ea37
Quinton Woodmansee <qwoodmansee@capital.edu>
2023-02-19 12:37:58 -0500

    clean up sanity client config

--- data-access-layer/sanity/sanity.client.ts
+++ data-access-layer/sanity/sanity.client.ts
@@ -1,9 +1,9 @@
-import { createClient, groq } from 'next-sanity';
+import { createClient } from 'next-sanity';
 import { cache } from 'react';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION; // "2022-11-16"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 const client = createClient({
   projectId,
