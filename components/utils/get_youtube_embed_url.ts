4f423cefd12c51d6d689c0cc1f6d383b7c66ed95
Quinton Woodmansee <qwoodmansee@capital.edu>
2023-02-25 20:52:02 -0500

    extract legacy embed url helper

---
+++ components/utils/get_youtube_embed_url.ts
@@ -0,0 +1,33 @@
+/* Legacy function from an older version which may be helpful in the future */
+export const getEmbedUrl = (videoUrl: string) => {
+  let result = videoUrl;
+  const EMBED_PREFIX = 'https://www.youtube.com/embed/';
+
+  const isShortUrl = videoUrl.indexOf('.be/') !== -1;
+  if (isShortUrl) {
+    result = EMBED_PREFIX + videoUrl.split('.be/')[1];
+  }
+
+  const hasVQueryParam = result.indexOf('v=') !== -1;
+
+  // remove the list query param if it it's there
+  const hasListQueryParam = result.indexOf('list=') !== -1;
+
+  // for some reason embed syntax makes you change t= to start=
+  result = result.replace('&t=', '&start=');
+  result = result.replace('?t=', '?start=');
+
+  if (hasVQueryParam || hasListQueryParam) {
+    const urlParams = new URLSearchParams(result.split('?')[1]);
+
+    // also have to remove the 's' after the number of seconds in the v query param syntax.
+    let startTimeAddition = '';
+    if (urlParams.has('start')) {
+      const timeWithoutS = urlParams.get('start').split('s')[0];
+      startTimeAddition = `?start=${timeWithoutS}`;
+    }
+    result = EMBED_PREFIX + urlParams.get('v') + startTimeAddition;
+  }
+
+  return result;
+};
