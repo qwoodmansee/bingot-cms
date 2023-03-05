372d3e2b7bee505f91d1c4b64f39811f98735e61
Quinton Woodmansee <qwoodmansee@capital.edu>
2023-03-04 23:36:12 -0500

    very basic game entity

--- domain-import-only/Game.ts
+++ domain-import-only/Game.ts
@@ -1,5 +1,29 @@
-export interface Game {
+import { Entity } from './utility-classes/Entity';
+
+interface GameProps {
   name: string;
   slug: string;
   coverArt: string;
+}
+
+export class Game extends Entity<GameProps> {
+  private constructor(props: GameProps) {
+    super(props, props.name);
+  }
+
+  public static create(props: GameProps): Game {
+    return new Game(props);
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get slug(): string {
+    return this.props.slug;
+  }
+
+  get coverArt(): string {
+    return this.props.coverArt;
+  }
 }
