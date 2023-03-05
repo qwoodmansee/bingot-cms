260a2c20335299424d68227944785ca75d0dcaf2
Quinton Woodmansee <qwoodmansee@capital.edu>
2023-03-04 23:35:02 -0500

    goal entity updates and data access layers

--- domain-import-only/Goal.ts
+++ domain-import-only/Goal.ts
@@ -1,7 +1,31 @@
 import { Trick } from './Trick';
+import { Entity } from './utility-classes/Entity';
 
-export interface Goal {
+export interface IGoalProps {
   name: string;
   notes: string;
-  tricks: Array<Trick>;
+  tricks: Trick[];
+}
+
+export class Goal extends Entity<IGoalProps> {
+  private constructor(props: IGoalProps) {
+    super(props, props.name);
+  }
+
+  public static create(props: IGoalProps): Goal {
+    const tricks = props.tricks.map((trick) => Trick.create(trick));
+    return new Goal({ ...props, tricks });
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get notes(): string {
+    return this.props.notes;
+  }
+
+  get tricks(): Trick[] {
+    return this.props.tricks;
+  }
 }
